d63cb788145c412cd170dd9d0496e37d386b64cf
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2023-06-27 00:13:22 +0100

    ...

---
+++ server/sampleData.js
@@ -0,0 +1,80 @@
+//Projects
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "eCommerce Website",
+    description:
+      "Lorem Ipsum dolor sit amet,Lorem Ipsum dolor sit amet,Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit ametLorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit ametLorem Ipsum dolor sit amet",
+    status: "In Progress",
+  },
+
+  {
+    id: "2",
+    clientId: "2",
+    name: "Dating App",
+    description:
+      "Lorem Ipsum dolor sit amet,Lorem Ipsum dolor sit amet,Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit ametLorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit ametLorem Ipsum dolor sit amet",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "SEO Project",
+    description:
+      "Lorem Ipsum dolor sit amet,Lorem Ipsum dolor sit amet,Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit ametLorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit ametLorem Ipsum dolor sit amet",
+    status: "In Progress",
+  },
+  {
+    id: "4",
+    clientId: "4",
+    name: "Design Prototype",
+    description:
+      "Lorem Ipsum dolor sit amet,Lorem Ipsum dolor sit amet,Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit ametLorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit ametLorem Ipsum dolor sit amet",
+    status: "Done",
+  },
+  {
+    id: "5",
+    clientId: "5",
+    name: "Auction Website",
+    description:
+      "Lorem Ipsum dolor sit amet,Lorem Ipsum dolor sit amet,Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit ametLorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit ametLorem Ipsum dolor sit amet",
+    status: "In Progress",
+  },
+];
+
+//Clients
+const clients = [
+  {
+    id: "1",
+    name: "Tony Stark",
+    email: "tonystark@gmail.com",
+    phone: "342-344-4444",
+  },
+  {
+    id: "2",
+    name: "Natasha Romanaova",
+    email: "natasha@gmail.com",
+    phone: "323-344-4444",
+  },
+  {
+    id: "3",
+    name: "Thor Odinson",
+    email: "odinson@gmail.com",
+    phone: "345-344-4444",
+  },
+  {
+    id: "4",
+    name: "Steve Rogers",
+    email: "rogers@gmail.com",
+    phone: "387-344-4444",
+  },
+  {
+    id: "5",
+    name: "Bruce Banner",
+    email: "bruce@gmail.com",
+    phone: "311-344-4444",
+  },
+];
+
+module.exports = { projects, clients };
